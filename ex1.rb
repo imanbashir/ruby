8c9a6bc2a209e587797b9d9d14f172006d95cb82
imanbashir <49904459+imanbashir@users.noreply.github.com>
2019-04-23 12:04:24 -0400

    Add files via upload

---
+++ ex1.rb
@@ -0,0 +1,11 @@
+#Learning Ruby the Hard Way
+#EXERCISE 1: A GOOD FIRST PROGRAM
+#Iman Bashir, April 22
+
+puts "Hello World!"
+puts "Hello Again"
+puts "I like typing this."
+puts "This is fun."
+puts "Yay! Printing."
+puts "I'd much rather you 'not'"
+#puts 'I "said" do not touch this'
---
+++ ex7.rb
@@ -0,0 +1,26 @@
+#Learning Ruby the Hard Way
+#EXERCISE 7: MORE PRINTING
+#Iman Bashir, April 22
+
+puts "Mary had a little lamb"
+puts "Its fleece was white as #{'snow'}."
+puts "And everywhere that Mary went."
+puts "." * 10 #What did that do?
+#it multiplied the dot by 10
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# watch that print vs. puts on this line what's it do?
+print end1 + end2 + end3 + end4 + end5 + end6
+puts end7 + end8 + end9 + end10 + end11 + end12
+#Ok so puts line breaks and print does not
